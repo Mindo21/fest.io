fff4a13e72570d5727c54ae0d616b9752c96fa2b
Matus Minarovic <matusm44@gmail.com>
2019-05-10 13:14:31 +0100

    edited port in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const PORT = process.env.PORT || "8080";
 
 // utils
 const config = require('./config.js');
